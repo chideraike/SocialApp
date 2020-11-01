b655f7fee8f3b28793ca174a8b8d09a08c4ec606
Chidera <42821245+chideraike@users.noreply.github.com>
2020-11-01 17:33:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
 export default FirebaseKeys = {
-    apiKey: "AIzaSyBk6HiYFZHOvmd_X5dzXu_wfpnH7vglfKE",
-    authDomain: "socialapp-6dbc8.firebaseapp.com",
-    databaseURL: "https://socialapp-6dbc8.firebaseio.com",
-    projectId: "socialapp-6dbc8",
-    storageBucket: "socialapp-6dbc8.appspot.com",
-    messagingSenderId: "891955550806",
-    appId: "1:891955550806:web:aeae7abe41461aa474b9f7"
+    // you're firebase keys to use
 };
 
 //!firebase.apps.length ? firebase.initializeApp(FirebaseKeys) : firebase.app();
